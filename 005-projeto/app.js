71563c6b3809e22bfeb04dddd97b3198a5952d18
Weslley334 <110498494+Weslley334@users.noreply.github.com>
2022-08-15 20:31:04 -0300

    Add files via upload

---
+++ 005-projeto/app.js
@@ -0,0 +1,9 @@
+var srt01 = "1" + 2 + 2;
+var srt02 = 1 + 2 + 2;
+var srt03 = 1 + 2 + "2";
+var srt04 = 1 + "2" + 2
+
+console.log(srt01);
+console.log(srt02);
+console.log(srt03);
+console.log(srt04);
