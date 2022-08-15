40ef95d8c65480e0aa660c9d3412fc468da83e7c
Weslley334 <110498494+Weslley334@users.noreply.github.com>
2022-08-15 20:10:04 -0300

    Add files via upload

---
+++ 005-projeto/app.js
@@ -0,0 +1,16 @@
+var num1 = 5;
+var num2 = 2;
+var num3;
+
+console.log(num3);
+
+console.log(num3=num1/num2);
+console.log(num1,num2,num3);
+num4 = num1%num2;
+console.log(num4);
+num3_round=Math.round(num3);
+
+console.log(num3_round);
+var aimds = 44444444.455545455
+var aimds = aimds.toFixed(1);
+console.log(aimds);
